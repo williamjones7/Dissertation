cc39aff83fbd05d22b984ebef6a37d644aefc8b8
Will Jones <williamjones0701@gmail.com>
2023-07-16 18:32:31 +0100

    update

--- FRSolver.py
+++ FRSolver.py
@@ -24,6 +24,31 @@ def maxDist(rs):
             d = max(d, np.linalg.norm(rs[i]-rs[j]))
     return d
 
+def findR(v0s, E0, masses):
+    ke = np.sum(KineticEnergy(v0s, masses))
+    R = 5 / (2 * (ke - E0))
+    return R
+
+def distCalculator(rs):
+    N = len(rs)
+    Rs = []
+    for i in range(N):
+        for j in range(i + 1, N):
+            Rs.append(np.linalg.norm(rs[i] - rs[j]))
+    Rs = np.array(Rs)
+    return Rs[:-1] / np.sum(Rs)
+
+def fillGrid(path, gridsize = 1000):
+    grid = np.zeros((gridsize, gridsize))
+
+    for pos in path:
+
+        grid_x = int(2 * (pos[0] - 1e-15) * gridsize)
+        grid_y = int(2 * (pos[1] - 1e-15) * gridsize)
+
+        grid[grid_x][grid_y] = 1
+
+    return grid
 
 def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t0 = 0):
     
@@ -34,18 +59,17 @@ def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t
     h = h0
     
     E0 = TotalEnergy(r0s, v0s, G, masses)
-
-    t_vals = [t0]
-    rs_traj = [r0s] 
-    vs_traj = [v0s] 
-    E_traj = [E0]
-    am_traj = [AngMomentum(rs, vs, masses)]
-    times = 0 
-
     E0hat = E0 
     ## conditions to avoid dividing by zero 
     if E0hat == 0: E0hat = np.max(np.abs(TotalKE(v0s, masses) + np.abs(r0s, G, masses))) 
     if E0hat == 0: E0hat = 1
+
+    t_vals = [t0]
+    rs_traj = [r0s] 
+    vs_traj = [v0s] 
+    E_traj = [0]
+    am_traj = [AngMomentum(rs, vs, masses)]
+    times = 0 
 
     rs, vs = Optimised_FR_Step(rs, vs, h, G, masses) 
     
@@ -65,7 +89,7 @@ def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t
         times += time.time() - t1
 
         E = TotalEnergy(rs, vs, G, masses) # Calculate Energy 
-        relE = np.abs(np.sum(E) - E0) / np.abs(E0hat)
+        relE = np.abs((E - E0) / E0hat)
 
         if h_new < hlim: 
             stability = 2
@@ -87,7 +111,7 @@ def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t
         rs_traj = rs_traj + [rs] 
         vs_traj = vs_traj + [vs]
         E_traj.append(relE)
-        am_traj.append(AngMomentum(rs, vs))
+        am_traj.append(AngMomentum(rs, vs, masses))
 
     # Make trajectories into numpy arrays
     rs_traj = np.array(rs_traj)
@@ -153,14 +177,14 @@ def optimisedSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.
     
     return stability,  maxE, t
 
-
-def shapeSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t0 = 0):
+def shapeSolver(T, C, r0s, v0s, G, masses, defaultVar = 10000, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t0 = 0):
     
     # Make a copy of initial values
     rs = np.copy(r0s)
     vs = np.copy(v0s)
     t = t0
     h = h0
+    variance = defaultVar
     
     E0 = TotalEnergy(r0s, v0s, G, masses)
     maxE = 0
@@ -193,6 +217,9 @@ def shapeSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, 
             break 
         if t+h_new > T: 
             stability =  1
+            path = [distCalculator(rs) for rs in rs_traj]
+            grid = fillGrid(path, gridsize = 1000)
+            variance = np.sum(grid)
             break 
         if relE > Elim:
             stability = 3
@@ -209,4 +236,5 @@ def shapeSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, 
     # Make trajectories into numpy arrays
     rs_traj = np.array(rs_traj)
     
-    return stability,  maxE, t
+    return stability, variance, maxE, t
+
