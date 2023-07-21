084f6e67b95c5dc62662c30419344dd0ceb791cd
William Jones <114030480+williamjones7@users.noreply.github.com>
2023-07-21 12:24:26 +0100

    update

--- FRSolver.py
+++ FRSolver.py
@@ -50,7 +50,7 @@ def fillGrid(path, gridsize = 1000):
 
     return grid
 
-def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t0 = 0):
+def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 1e-10, t0 = 0):
     
     # Make a copy of initial values
     rs = np.copy(r0s)
@@ -120,7 +120,7 @@ def fullSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t
     return (t_vals, rs_traj, vs_traj, E_traj, am_traj, times), stability 
 
 
-def optimisedSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t0 = 0):
+def optimisedSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 1e-10, t0 = 0):
     
     # Make a copy of initial values
     rs = np.copy(r0s)
@@ -177,7 +177,7 @@ def optimisedSolver(T, C, r0s, v0s, G, masses, hlim = 1e-3, Elim = 0.01, h0 = 0.
     
     return stability,  maxE, t
 
-def shapeSolver(T, C, r0s, v0s, G, masses, defaultVar = 10000, hlim = 1e-3, Elim = 0.01, h0 = 0.01, t0 = 0):
+def shapeSolver(T, C, r0s, v0s, G, masses, defaultVar = 10000, hlim = 1e-3, Elim = 0.01, h0 = 1e-10, t0 = 0):
     
     # Make a copy of initial values
     rs = np.copy(r0s)
@@ -237,4 +237,79 @@ def shapeSolver(T, C, r0s, v0s, G, masses, defaultVar = 10000, hlim = 1e-3, Elim
     rs_traj = np.array(rs_traj)
     
     return stability, variance, maxE, t
+
+
+
+def NStepsSolver(NSteps, C, r0s, v0s, G, masses, hlim = 1e-6, Elim = 0.01, h0 = 1e-10, t0 = 0):
+    
+    # Make a copy of initial values
+    rs = np.copy(r0s)
+    vs = np.copy(v0s)
+    t = t0
+    h = h0
+    
+    E0 = TotalEnergy(r0s, v0s, G, masses)
+    E0hat = E0 
+    ## conditions to avoid dividing by zero 
+    if E0hat == 0: E0hat = np.max(np.abs(TotalKE(v0s, masses) + np.abs(r0s, G, masses))) 
+    if E0hat == 0: E0hat = 1
+
+    t_vals = [t0]
+    rs_traj = [r0s] 
+    vs_traj = [v0s] 
+    E_traj = [0]
+    am_traj = [AngMomentum(rs, vs, masses)]
+    times = 0 
+
+    totalSteps = 0
+
+    rs, vs = Optimised_FR_Step(rs, vs, h, G, masses) 
+    
+    # run scheme for requried number of steps 
+    while 1 < 2:
+        t1 = time.time()
+
+        h = C * findh(rs, vs) # proposed timestep
+
+        rs_bar, vs_bar = Optimised_FR_Step(rs, vs, h, G, masses)
+        h_bar = C * findh(rs_bar, vs_bar) # proposed timestep
+
+        h_new = (h + h_bar) / 2
+        
+        rs, vs = Optimised_FR_Step(rs, vs, h_new, G, masses)
+
+        times += time.time() - t1
+
+        E = TotalEnergy(rs, vs, G, masses) # Calculate Energy 
+        relE = np.abs((E - E0) / E0hat)
+
+        if h_new < hlim: 
+            stability = 2
+            break 
+        if totalSteps > NSteps: 
+            stability =  1
+            break 
+        if relE > Elim:
+            stability = 3
+            break 
+        if maxDist(rs) > 10:
+            stability = 0
+            break 
+        
+        t += h_new
+
+        ## append values to trajectories 
+        t_vals = t_vals + [t]
+        rs_traj = rs_traj + [rs] 
+        vs_traj = vs_traj + [vs]
+        E_traj.append(relE)
+        am_traj.append(AngMomentum(rs, vs, masses))
+
+        totalSteps += 1
+
+    # Make trajectories into numpy arrays
+    rs_traj = np.array(rs_traj)
+    vs_traj = np.array(vs_traj) 
+    
+    return (t_vals, rs_traj, vs_traj, E_traj, am_traj, times), stability 
 
